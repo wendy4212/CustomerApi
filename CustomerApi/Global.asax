5c4df42e50d15844c11ec6c71a6a291abd9aaaba
BradYu <BradYu@DESKTOP-B3MHNS0>
2017-05-21 09:50:03 +0800

    加入專案檔案。

---
+++ CustomerApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerApi.MvcApplication" Language="C#" %>
---
+++ CustomerApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerApi.Startup))]
+namespace CustomerApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
